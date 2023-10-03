6e8fb4e74e154f846b6c105a166ae535ce7685c1
didac <contact@masteringbox.com>
2023-10-03 11:49:25 +0200

    Fetch Bug Example. Everything you need to look at is in app/[slug]/page.jsx
    Build Issue with Fetch being called multiple times.
    - In nextjs 13.5.3 and Node 18.2.1 (Vercel version) the API gets called TWICE for every single page on build. I get one Fetch error per page. 11 API calls for 5 pages.
    - In node 20.6.1 it is still called Twice per page on build but fetch error doesn't occur.
    - In nextjs 13.4.3 works ok in local with Node 20.6.1 and 18.2.1. Will deploy to Vercel now.
    
    Update:
    - Updating to 13.5.3 to showcase the nextjs issue. Vercel deployment with nextjs 13.4.3 caused 2 API requests instead of 1 that should happen (But not 11 Like before).
    - In a more complex application API call still gets called like crazy. Check my repository "fetch-cache-bug" for a little bit more complex project with categories/blog/tags.

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -23,9 +23,10 @@ export default async function page({params: {slug}}) {
     const posts = await getData();
     /**
      *  This should always hit fetch cache in build.
-     *  In 13.5.3 and Node 18.2.1 (Vercel version) the API gets called TWICE for every single page on build. I get one Fetch error per page.
+     *  In 13.5.3 and Node 18.2.1 (Vercel version) the API gets called TWICE for every single page on build. I get one Fetch error per page. In a 5 SSG page build it gets called 11 times.
      *  In node 20.6.1 it is still called Twice per page on build but fetch error doesn't occur.
-     *  In 13.4.3 works ok in local with Node 20.6.1.
+     *  In 13.4.3 works ok in local with Node 20.6.1 and Node 18.2.1. In Vercel it does 2 API calls instead of 1 (but not 11).
+     *  In a more complex app, Vercel does a lot of unnecessary calls. You can check a project that is a little bit more complex https://github.com/MasteringBOX/fetch-cache-bug to showcase the issue.
      */
     const post = posts.find(post => post.slug === slug);
     return <div>{post.title.rendered}</div>
